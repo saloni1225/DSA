949288336144ada45151077349feb37e616a8e5a
Saloni Kumari <saloni01april@gmail.com>
2026-08-18 13:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 31.8 MB (56.37%) - LeetSync

--- 4316-minimum-swaps-to-move-zeros-to-end/minimum-swaps-to-move-zeros-to-end.cpp
+++ 4316-minimum-swaps-to-move-zeros-to-end/minimum-swaps-to-move-zeros-to-end.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int minimumSwaps(vector<int>& nums) {
-        int size=nums.size();
         int i=0;
-        int j=size-1;
+        int j=nums.size()-1;
         int count=0;
         while(i<j){
-            while(i<size && nums[i]!=0){
-                i++;
-            }
-            while(j>0 && nums[j]==0){
-                j--;
-            }
-            if(i>=j){
-                break;
-            }
-            count++;
+            while(i<j && nums[i]!=0){
             i++;
+        }
+        while(j>i && nums[j]==0) {
             j--;
         }
-        return count;
+        if(i>=j){
+            break;
+        }
+    count++;
+    i++;
+    j--;
+    }
+    return count;
     }
 };
