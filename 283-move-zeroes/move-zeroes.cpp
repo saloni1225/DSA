5271c6b626e5aa5fefe4329f4074b6e2beb8fa9b
Saloni Kumari <saloni01april@gmail.com>
2026-08-18 13:00:03 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (83.22%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i,j=0;
+        for(i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+        
+    }
+};
