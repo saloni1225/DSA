a36d59a64454bff737df81c03882f5b890da2bfa
Saloni Kumari <saloni01april@gmail.com>
2026-08-20 22:30:36 +0530

    Time: 400 ms (5.00%) | Memory: 21.8 MB (38.89%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        for(int i=0;i<nums.size()+1;i++){
+            int flag=0;
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]==i){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)return i;
+        }
+        return -1;
+    }
+};
